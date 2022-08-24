c2542cc3cd051e587ce43d89fe938b802fee7526
Leandro Bove <leandrobove@hotmail.com>
2022-08-24 14:32:24 -0300

    Correção de organização de pacotes

--- src/main/java/br/com/leandrobove/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/br/com/leandrobove/core/modelmapper/ModelMapperConfig.java
@@ -1,4 +1,4 @@
-package br.com.leandrobove.core.config.modelmapper;
+package br.com.leandrobove.core.modelmapper;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
