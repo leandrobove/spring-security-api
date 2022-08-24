192773e5dcf6a587172a9a96d85c25db0b04f58f
Leandro Bove <leandrobove@hotmail.com>
2022-08-24 14:26:17 -0300

    Alterando nome do projeto

--- src/main/java/br/com/leandrobove/SpringSecurityTestApplication.java
+++ src/main/java/br/com/leandrobove/SpringSecurityTestApplication.java
@@ -11,10 +11,10 @@ import br.com.leandrobove.domain.service.RoleService;
 import br.com.leandrobove.domain.service.UserService;
 
 @SpringBootApplication
-public class SpringsecuritytestApplication {
+public class SpringSecurityTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecuritytestApplication.class, args);
+		SpringApplication.run(SpringSecurityTestApplication.class, args);
 	}
 
 	@Bean
